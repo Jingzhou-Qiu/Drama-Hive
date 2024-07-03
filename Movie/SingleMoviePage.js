e6157609a10e9b46f4283cbb25de33827d2e5529
Jingzhou-Qiu <yzp7fe@virginia.edu>
2024-07-03 12:15:50 -0400

    implment movie review

--- Movie/SingleMoviePage.js
+++ Movie/SingleMoviePage.js
@@ -1,7 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { Text, StyleSheet, Image, View, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState, useContext, useCallback } from 'react';
+import { Text, StyleSheet, Image, View, TouchableOpacity, ScrollView, FlatList } from 'react-native';
 import { options, screenStyle } from '../MyContext/ConstantContext';
 import { useNavigation } from '@react-navigation/native';
+import { getDataWithFilter } from '../MyContext/Firebase';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { useFocusEffect } from '@react-navigation/native';
 
 
 const baseUrl = 'https://image.tmdb.org/t/p/original';
@@ -39,18 +42,20 @@ const MovieInfo = ({ title, tagline, smallTag }) => (
 );
 
 
-const ActionButtons = () => {
+
+const ActionButtons = ({ id }) => {
     const navigation = useNavigation();
     const writeReview = () => {
-        navigation.navigate("WriteReview")
+        navigation.navigate("WriteReview", { id, type: "movie" })
     }
+
 
     return (
         <View style={styles.buttonContainer}>
             <TouchableOpacity style={styles.button}>
                 <Text style={styles.buttonText}>Add</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.button} onPress={ writeReview }>
+            <TouchableOpacity style={styles.button} onPress={writeReview}>
                 <Text style={styles.buttonText}>Watched</Text>
             </TouchableOpacity>
         </View>
@@ -62,6 +67,7 @@ export default function SingleMoviePage({ route }) {
     const [detail, setDetail] = useState(null);
     const [smallTag, setSmallTag] = useState('');
     const [isLoading, setIsLoading] = useState(true);
+    const [review, setReview] = useState(null)
     const { movie } = route.params;
     const imageUrl = baseUrl + movie.poster_path;
 
@@ -82,14 +88,19 @@ export default function SingleMoviePage({ route }) {
 
     useEffect(() => {
         fetchDetails(movie.id);
+        getDataWithFilter("Review", "id", '==', movie.id).then(rs => setReview(rs))
     }, [movie.id]);
+
+    useFocusEffect(
+        useCallback(()=>{getDataWithFilter("Review", "id", '==', movie.id).then(rs => setReview(rs))},[])
+    )
 
     if (isLoading) {
         return <Text>Loading...</Text>;
     }
 
     return (
-        <View style={screenStyle.container}>
+        <ScrollView style={screenStyle.container}>
             <View style={styles.firstContainer}>
                 <MoviePoster imageUrl={imageUrl} />
                 <MovieInfo
@@ -98,18 +109,106 @@ export default function SingleMoviePage({ route }) {
                     smallTag={smallTag}
                 />
             </View>
-            <ActionButtons />
+            <ActionButtons id={movie.id} />
             {movie.overview && (
                 <Text style={styles.text_overview}>
                     Overview: {movie.overview}
                 </Text>
             )}
-            <Text style={styles.titleText}>User review: to be implemented</Text>
-        </View>
+            <ReviewComponent data={review} />
+        </ScrollView>
     );
 }
+const Star = ({ filled, size }) => (
+    <TouchableOpacity style={styles.starButton}>
+        <Icon
+            name={filled ? 'star' : 'star-o'}
+            size={size}
+            color={filled ? 'gold' : 'lightgray'}
+        />
+    </TouchableOpacity>
+);
+
+const ReviewComponent = ({ data }) => {
+
+    const renderItem = ({ item }) => (
+        <View style={styles.reviewContainer}>
+            <View style={styles.userInfo}>
+                <Text style={styles.username}>{item.user}</Text>
+            </View>
+
+            <View style={styles.reviewContent}>
+                <View style={[{ flexDirection: "row" }, {marginBottom:10}]}>
+                    {[1, 2, 3, 4, 5].map((star) => (
+                        <Star
+                            key={star}
+                            filled={star <= item.rating}
+                            size={10}
+                        />
+                    ))}
+                </View>
+                <Text style={styles.reviewText}>{item.review}</Text>
+            </View>
+        </View>
+    );
+    if (data){
+        return (
+            <View style={styles.listContainer}>
+            {
+                data.map((item)=>renderItem({item}))
+            }
+            </View>
+        );
+
+    }
+};
+
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    listContainer: {
+        paddingHorizontal: 16,
+    },
+    reviewContainer: {
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        padding: 16,
+        marginVertical: 8,
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        shadowOffset: { width: 0, height: 2 },
+        elevation: 3,
+    },
+    profileImage: {
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        marginRight: 10,
+    },
+    userInfo: {
+        flex: 1,
+    },
+    username: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    date: {
+        color: '#888',
+        fontSize: 12,
+    },
+    reviewContent: {
+        marginTop: 5,
+    },
+    reviewText: {
+        fontSize: 14,
+        color: '#333',
+    },
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'flex-end',
